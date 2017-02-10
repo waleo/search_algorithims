2b44ec7ac499d7be617805a86bcf2b60dc49ff30
waleo <info@railsfever.com>
2017-02-09 23:38:28 -0500

    dfs first cut

--- driver_3.py
+++ driver_3.py
@@ -109,20 +109,8 @@ class Driver(object):
       if self.goalTest(state):
         duration = time.time() - t
         ramUsed = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / (1024 * 1024)
+        self.__print_results__(state,frontier,explored,duration, nodes_expanded, ramUsed)
         print('SUCCESS')
-        #print("MOVES: ", state.getMoves())
-        f = open("output.txt", "w")
-        shortestPath = self.findShortestPath(state)
-        f.write('path_to_goal: ' + str(shortestPath) + '\n')
-        f.write('cost_of_path: ' + str(len(shortestPath)) + '\n')
-        f.write('nodes_expanded: ' + str(nodes_expanded) + '\n')
-        f.write('fringe_size: ' + str(len(frontier)) + '\n')
-        f.write('max_fringe_size: ' + str(len(frontier) + 1) + '\n')
-        f.write('search_depth: ' + str(self.findSearchDepth(state)) + '\n')
-        f.write('max_search_depth: ' + str(self.maxSearchDepth(frontier)) + '\n')
-        f.write('running_time: ' + str(duration) + '\n')
-        f.write('max_ram_usage: ' + str(ramUsed) + '\n')
-        f.close()
         return
       
       children = state.children()
@@ -140,6 +128,61 @@ class Driver(object):
     import sys
     sys.exit("ALGORITHM FAILURE")
     return
+
+  def dfs(self,initialState):
+    from collections import deque
+    initialList = initialState.split(",")
+    startState = BoardTile(initialList)
+    frontier = deque()
+    frontier.append(startState)
+    explored = deque()
+    nodes_expanded = 0
+
+    import resource
+    import time
+    t = time.time()
+    while not len(frontier) == 0:
+      state = frontier.popleft()
+      explored.append(state)
+      print("CURRENTLY EXPLORING...", state)
+      print("EXPLORED: ", *explored)
+
+      if self.goalTest(state):
+        duration = time.time() - t
+        ramUsed = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / (1024 * 1024)
+        self.__print_results__(state,frontier,explored,duration, nodes_expanded, ramUsed)
+        print('SUCCESS')
+        return
+      
+      children = state.children()
+      children.reverse()
+      nodes_expanded = nodes_expanded + 1 
+      for child in children:
+        print("CHILD: ", child.getStart())
+
+        if child in frontier or child in explored:
+          pass
+        else:
+          #print("Appending: ", child)
+          frontier.append(child)
+      print("FRONTIER: ", *frontier)
+
+    import sys
+    sys.exit("ALGORITHM FAILURE")
+
+  def __print_results__(self,state,frontier,explored, duration, nodes_expanded, ramUsed):
+    f = open("output.txt", "w")
+    shortestPath = self.findShortestPath(state)
+    f.write('path_to_goal: ' + str(shortestPath) + '\n')
+    f.write('cost_of_path: ' + str(len(shortestPath)) + '\n')
+    f.write('nodes_expanded: ' + str(nodes_expanded) + '\n')
+    f.write('fringe_size: ' + str(len(frontier)) + '\n')
+    f.write('max_fringe_size: ' + str(len(set(explored).union(frontier))) + '\n')
+    f.write('search_depth: ' + str(self.findSearchDepth(state)) + '\n')
+    f.write('max_search_depth: ' + str(self.maxSearchDepth(frontier)) + '\n')
+    f.write('running_time: ' + str(duration) + '\n')
+    f.write('max_ram_usage: ' + str(ramUsed) + '\n')
+    f.close()
 
   def goalTest(self, state):
     #print("START: ", state.getStart())
@@ -173,3 +216,5 @@ board_string = sys.argv[2]
 driver = Driver()
 if search_type == 'bfs':
   driver.bfs(board_string)
+elif search_type == 'dfs':
+  driver.dfs(board_string)
